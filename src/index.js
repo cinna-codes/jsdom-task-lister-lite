74520741f6a689452195cdc81cb1d3ee40912c26
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-12 17:02:32 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+  //     document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+  //     event.preventDefault();
+  // }, false);
+  document.querySelector("input[type=submit]").addEventListener("click", function(event) {
+    event.preventDefault;
+  })
 });
